e2af10625b003d00c9f1c3eac57e090c94ea2ced
juanAvila <juan_da89@hotmail.com>
2020-10-01 20:38:50 -0500

    Agregar archivos de proyecto.

---
+++ Example/Example/Base.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Example
+{
+    public abstract class Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string nameProperty = null)=>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameProperty));
+    }
+}
---
+++ Example/Example/Maquinaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public class Maquinaria:Base
+    {
+        public Maquinaria() {
+            var aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var hour = DateTime.Now;
+            Hora = hour.Hour;
+            Minuto = hour.Minute;
+            Segundo = hour.Second;
+        }
+        private int _hora;
+        public int Hora
+        {
+            get { return _hora; }
+            set { _hora = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _minuto;
+        public int Minuto
+        {
+            get { return _minuto; }
+            set { _minuto = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _segundo;
+        public int Segundo
+        {
+            get { return _segundo; }
+            set { _segundo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
